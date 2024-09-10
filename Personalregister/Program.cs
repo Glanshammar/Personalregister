898658669ee78b2c53f6a6f2af035b303ea7b1ea
Andreas Johansson <glanshammar70@gmail.com>
2024-09-10 15:19:51 +0200

    Added the command input loop.

--- Personalregister/Program.cs
+++ Personalregister/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<Employee> firstlist = new List<Employee>();
+        while (true)
+        {
+            Console.Write(">> ");
+            string input = Console.ReadLine() ?? string.Empty;
+            if (input == "exit")
+            {
+                break;
+            }
+
+            if (input == "help")
+            {
+                Console.WriteLine("Commands:");
+                Console.WriteLine("  list - list all employees.");
+                Console.WriteLine("  add - add employee to the system.");
+                Console.WriteLine("  exit - exit the program.");
+            }
+            
+            if (input.ToLower() == "list")
+            {
+                Commands.ListEmployees();
+            }
+            
+            else if (input.ToLower() == "add")
+            {
+                Commands.AddEmployee();
+            }
+        }
     }
 }
