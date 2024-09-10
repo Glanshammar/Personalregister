aec1936fb29983bd097c1efa37211310ad3d0d0c
Andreas Johansson <glanshammar70@gmail.com>
2024-09-10 14:13:31 +0200

    Initial commit

---
+++ Personalregister/Employee.cs
@@ -0,0 +1,18 @@
+﻿public class Employee{
+    private static string firstName;
+    private static string lastName;
+    private static Int32 salary;
+
+    Employee(string firstName, string lastName, Int32 salary)
+    {
+        firstName = firstName;
+        lastName = lastName;
+        salary = salary;
+    }
+
+    public static void PrintEmployee()
+    {
+        Console.WriteLine("\nEmployee Information:");
+        Console.WriteLine("First Name: {0}", firstName);
+    }
+}
---
+++ Personalregister/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Personalregister;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Employee> firstlist = new List<Employee>();
+    }
+}
